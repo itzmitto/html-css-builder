8bc19feb351160603937cd10140f232ee8ebbd0d
itzmitto <andre.babirian2004@gmail.com>
2026-08-08 20:23:51 +0200

    Add NavbarPreview component to display a navigation bar with logo and links

---
+++ src/components/previews/NavbarPreview.tsx
@@ -0,0 +1,16 @@
+function NavbarPreview() {
+  return (
+    <div className="navbar-preview">
+      <div className="logo">Logo</div>
+
+      <div className="nav-links">
+        <span>Home</span>
+        <span>About</span>
+        <span>Services</span>
+        <span>Contact</span>
+      </div>
+    </div>
+  );
+}
+
+export default NavbarPreview;
