8867daf366407af59ce838c8732f7eba6fce5522
itzmitto <andre.babirian2004@gmail.com>
2026-08-09 22:00:26 +0200

    Add color prop to ButtonPreview component for customizable text color

--- src/components/previews/ButtonPreview.tsx
+++ src/components/previews/ButtonPreview.tsx
@@ -1,12 +1,19 @@
 interface ButtonPreviewProps {
   text?: string;
+  color?: string;
 }
 
 function ButtonPreview({
   text,
+  color,
 }: ButtonPreviewProps) {
   return (
-    <button className="button-preview">
+    <button
+      className="button-preview"
+      style={{
+        color: color || "#000000",
+      }}
+    >
       {text}
     </button>
   );
